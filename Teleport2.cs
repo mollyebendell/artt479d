5b23620bec3e726996130aa901115f46047c1df6
mollyebendell <mbendell1@gmail.com>
2018-10-25 16:54:59 -0400

    Add files via upload

---
+++ Teleport2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Teleport2 : MonoBehaviour
+{
+    public Transform destination;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Door")
+        {
+            FindObjectOfType<AudioManager1>().Play("DoorSound");
+        }
+        other.transform.position = destination.transform.position;
+
+    }
+}
