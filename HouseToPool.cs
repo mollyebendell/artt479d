2f2150ee611d1a261adf4f26ffa024a08d3a0620
mollyebendell <mbendell1@gmail.com>
2018-11-13 16:11:34 -0500

    Add files via upload
    In the build settings:
    
    File > Build Settings > Add Open Scenes
    - both scenes you want to use the script on must be in the Build Settings
    
    On your main character object:
    RigidBodyFPS > Inspector Window > Tag (drop down menu) > Player
    
    On the object you want to use as the trigger:
    Add Component > Box Collider > checkbox that says "Is Trigger"
    Add Component > HouseToPool (or whatever you named the script)

---
+++ HouseToPool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HouseToPool : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter (Collider other) {
+
+        if(other.tag == "Player"){
+            SceneManager.LoadScene("Scenes/MagicPool");
+        }
+		
+	}
+}
