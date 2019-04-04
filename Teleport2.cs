63d63de71578bea9f497746f4da6f9630523b7ea
mollyebendell <mbendell1@gmail.com>
2019-04-04 16:18:17 -0400

    Update Teleport2.cs

--- Teleport2.cs
+++ Teleport2.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class Teleport2 : MonoBehaviour
+[RequireComponent(typeof(AudioSource))]
+
+public class Teleport : MonoBehaviour
 {
     public Transform destination;
+    private AudioSource sound;
 
     void OnTriggerEnter(Collider other)
     {
         if (gameObject.tag == "Door")
         {
-            FindObjectOfType<AudioManager1>().Play("DoorSound");
+            sound = GetComponent<AudioSource>();
+            sound.Play();
+            Debug.Log("Sound played.");
+            
         }
         other.transform.position = destination.transform.position;
 
